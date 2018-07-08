7e3f8769577947a57506813b46c778ac903572a9
Michal_Win10 <mmaku@users.noreply.github.com>
2018-07-08 13:48:33 +0200

    Reorganization

---
+++ 08 benchOneFunctions.R
@@ -0,0 +1,126 @@
+# Written by Micha³ Makowski
+
+# install.packages("MASS")
+# install.packages("glasso")
+# install.packages("huge")
+
+require(MASS)
+require(glasso)
+require(huge)
+require(ggplot2)
+require(latex2exp)
+require(reshape2)
+
+source("01 auxilaryFunctions.R")
+source("04 admmGLASSO.R")
+source("07 admmGSLOPE.R")
+
+benchOne <- function(data, 
+                     alpha = .05, 
+                     penalizeDiagonal = F, 
+                     truncate = T, 
+                     epsilon = 10e-4, 
+                     mu = 1, 
+                     selectCriterion = "stars")
+{
+    sampleCovariance <- cov(data)
+    
+    n     <- nrow(data)
+    p     <- ncol(data)
+    m     <- p*(p-1)/2
+    mBanerjee <- p^2   
+    banerjeeLassoLambda <- qt(1-alpha/2/mBanerjee, df = n-2)/sqrt(n-2+qt(1-alpha/2/mBanerjee, df = n-2)^2)
+    
+    # gLASSO
+    gLassoADMM <- glassoADMM(sampleCovariance, mu = mu, lambda = banerjeeLassoLambda, 
+                             penalizeDiagonal = penalizeDiagonal, truncate = truncate, absoluteEpsilon = epsilon)
+    
+    k = 1:m
+    BHlambda <- qt(1-alpha*k/2/m, df = n-2)/sqrt(n-2+qt(1-alpha*k/2/m, df = n-2)^2)
+    BHSlopeLambda <- c(rep(BHlambda[1], p), rep(BHlambda, each=2))
+    # gSLOPE
+    gSlopeADMM <- gslopeADMM(sampleCovariance, mu = mu, lambda = BHSlopeLambda, 
+                             penalizeDiagonal = penalizeDiagonal, truncate = truncate, absoluteEpsilon = epsilon)
+    
+    # gLASSOstars
+    gLassoHuge <- huge(data, nlambda=30, method = "glasso")
+    lassoSelect <- huge.select(gLassoHuge, criterion = selectCriterion) # Select the graph using StARS
+    gLassoStARS <- glassoADMM(sampleCovariance, mu = mu, lambda = lassoSelect$opt.lambda, 
+                              penalizeDiagonal = penalizeDiagonal, truncate = truncate, absoluteEpsilon = epsilon)
+    
+    return(list(gLassoADMM = list(lassoParameter = gLassoADMM$lambda, 
+                                  precisionMatrix = gLassoADMM$precisionMatrix, 
+                                  residuals = gLassoADMM$residuals, 
+                                  iterations = gLassoADMM$iterations),
+                gSlopeADMM = list(lassoParameter = gSlopeADMM$lambda, 
+                                  precisionMatrix = gSlopeADMM$precisionMatrix, 
+                                  residuals = gSlopeADMM$residuals, 
+                                  iterations = gSlopeADMM$iterations),
+                gLassoStARS = list(lassoParameter = gLassoStARS$lambda, 
+                                   precisionMatrix = gLassoStARS$precisionMatrix, 
+                                   residuals = gLassoStARS$residuals, 
+                                   iterations = gLassoStARS$iterations),
+                sampleCovariance = sampleCovariance,
+                lagrangianParameter = mu,
+                diagonalPenalization = penalizeDiagonal,
+                truncated = truncate,
+                epsilon = epsilon,
+                alpha = alpha,
+                selectMethod = selectCriterion))
+}
+
+meltingMatrix <- function(matrix, 
+                          index = NULL)
+{
+    colnames(matrix) <- 1:ncol(matrix)
+    rownames(matrix) <- 1:ncol(matrix)
+    
+    matrix <- reshape2::melt(matrix)
+    
+    if(!is.null(index))
+        matrix <- cbind(matrix, index)
+    
+    return(matrix)
+}
+    
+plotBenchOne <- function(benchResult)
+{
+    properData <- rbind(meltingMatrix(benchResult[[1]]$precisionMatrix, "gLASSO"),
+                        meltingMatrix(benchResult[[2]]$precisionMatrix, "gSLOPE"),
+                        meltingMatrix(benchResult[[3]]$precisionMatrix, "gLASSO_StARS"),
+                        meltingMatrix(benchResult$sampleCovariance, "sampleCovariance"))
+
+    colnames(properData) <- c("X1", "X2", colnames(properData)[3:4])
+    
+    mainTitle <- paste0("Comparision of different precision matrix estimation methods")
+    
+    subTitle <- paste0('alpha = ', benchResult$alpha, 
+                        ', diagonal penalization: ', c('No', 'Yes')[1+benchResult$diagonalPenalization],
+                        ', values below ', benchResult$epsilon, ' truncated: ', c('No.', 'Yes.')[1+benchResult$truncated])
+    
+    properData
+    
+    out <- ggplot(properData, aes(x=X1, y=X2))
+    
+    if(benchResult$truncated)
+    {
+        out = out + geom_tile(data = subset(properData, !is.zero(value)), aes(fill = value)) + 
+            geom_tile(data = subset(properData,  is.zero(value)), aes(colour = "0"), linetype = 0, fill = "grey50", alpha = .5)
+    }
+    else
+    {
+        out = out + geom_tile(aes(fill = value))
+    }
+    
+    out = out + labs(title =  mainTitle, subtitle = subTitle, x = TeX('$X_1$'), y = TeX('$X_2$')) +
+        facet_wrap(~index) +
+        scale_fill_gradient(name="Matrix\nentry\nvalue",
+                            limits = c(.Machine$double.eps, NA)) +
+        scale_colour_discrete(name=NULL) +
+        guides(fill = guide_colorbar(order = 1, barwidth = 1, barheight = 10), 
+               colour = guide_legend(order = 2, keywidth = 1, keyheight = 1, title.position = "bottom")) +
+        theme_minimal() +
+        theme(legend.spacing.y = unit(-0.3, "cm"))
+
+    return(out)
+}
---
+++ 09 contingencyMeasures.R
@@ -0,0 +1,43 @@
+# Written by Micha³ Makowski
+
+# Contingency measures
+
+FP <- function(estimatedMatrix, realMatrix)
+{
+    # H_0: x_ij == 0
+    FP <- sum(estimatedMatrix[realMatrix == 0] != 0)
+
+    return(FP)
+}
+
+TP <- function(estimatedMatrix, realMatrix)
+{
+    # H_0: x_ij == 0
+    TP <- sum(estimatedMatrix[realMatrix != 0] != 0)
+    
+    return(TP)
+}
+
+FN <- function(estimatedMatrix, realMatrix)
+{
+    # H_0: x_ij == 0
+    FN <- sum(estimatedMatrix[realMatrix != 0] == 0)
+    
+    return(FN)
+}
+
+TN <- function(estimatedMatrix, realMatrix)
+{
+    # H_0: x_ij == 0
+    TN <- sum(estimatedMatrix[realMatrix == 0] == 0)
+    
+    return(TN)
+}
+
+FDP <- function(estimatedMatrix, realMatrix)
+{
+    # H_0: x_ij == 0
+    predictedPositive <- max(c(sum(estimatedMatrix != 0), 1))
+    
+    return(FP((estimatedMatrix, realMatrix))/predictedPositive)
+}
